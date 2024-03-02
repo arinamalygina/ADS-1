523ff58b71995f21815a67776d0223603f7b339d
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-03-02 23:23:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,44 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-	for (int i = 2; i * i <= value; i++) {
-		if (value % i == 0) {
-			return false;
-		}
-	}
-	return true;
+  for (int i = 2; i * i <= value; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int k = 0;
-	for (uint64_t i = 2; ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true) {
-			k++;
-			if (k == n);
-				return i;
-		}
-	}
+  int k = 0;
+  for (uint64_t i = 2;; i++) {
+    bool is_prime = checkPrime(i);
+    if (is_prime == true) {
+      k++;
+      if (k == n)
+        ;
+      return i;
+    }
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-	for (uint64_t i = value + 1; ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true)
-		return i;
-	}
+  for (uint64_t i = value + 1;; i++) {
+    bool is_prime = checkPrime(i);
+    if (is_prime == true) return i;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	for (uint64_t i = 2; i <= hbound ; i++) {
-		bool is_prime = checkPrime(i);
-		if (is_prime == true) {
-			sum += i;
-			
-		}
-	}
-	return sum;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i <= hbound; i++) {
+    bool is_prime = checkPrime(i);
+    if (is_prime == true) {
+      sum += i;
+    }
+  }
+  return sum;
 }
